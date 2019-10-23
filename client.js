d66cf1e6e63b752f8a940c737008381ba5fbc241
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-23 18:04:41 +0000

    tested that moving actions are working

--- client.js
+++ client.js
@@ -12,9 +12,14 @@ const connect = function() {
   conn.on('data', (data) => { //once connection established
     console.log('Server says: ', data); //recieve data
   });
-  conn.on('connect', ()=>{
-    console.log('Successflly connected to game server');
-    conn.write('Name: DAH');
+  conn.on('connect', ()=>{ //once connected to server
+    console.log('Successflly connected to game server'); //log to client
+    conn.write('Name: DAH'); //send to server
+    // conn.write('Move: up');
+    // setInterval(() => conn.write('Move: up'), 180);
+    // setInterval(()=> conn.write('Move: up'), 900);
+    // setTimeout(()=> conn.write('Move: left'), 50);
+    // setInterval(()=> conn.write('Move: right'), 650);
   });
   return conn;
 }

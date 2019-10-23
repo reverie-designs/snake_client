8307a3bb329030367c0ab3050e1cb2e4a3521493
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-23 18:49:18 +0000

    refractored setUpInput into a file to handle user input and required it in the main play file'

--- play.js
+++ play.js
@@ -1,35 +1,9 @@
 const net = require('net');
 const connect = require('./client');
+const setUpInput = require('./input');
 console.log('Connecting ...');
 connect();
+setUpInput();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const  handleUserInput = function(data){
-  // stdin.on(data => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  // }); 
-};
-const setupInput = function() {
-  const stdin = process.stdin;
-  
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const  handleUserInput = function(){
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    }); 
-  };
-  handleUserInput();
-  return stdin;
-};
-setupInput();
 
 
